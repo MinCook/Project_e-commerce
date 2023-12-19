a2a4c05cde8997915ff11de1fbd8e11e8ffe6898
MinCook <128447440+MinCook@users.noreply.github.com>
2023-12-19 11:05:22 +0700

    L6

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const multer  = require('multer')
 const router = express.Router();
-const storageMulter = require("../../helpers/storageMulter")
-const upload = multer({ storage:storageMulter() })
+const upload = multer()
 const controller = require("../../controllers/admin/product.controller")
 const validate = require("../../validates/admin/product.validate")
+const uploadCloud = require("../../middlewares/admin/upLoad.middlewares")
+
 router.get("/",controller.product );
 router.patch("/changeStatus/:status/:id",controller.changeStatus);
 router.patch("/changeMulti",controller.changeMulti);
 router.delete("/delete/:id",controller.deleteItem);
-router.post("/create", upload.single('thumbnail'),validate.createPost ,controller.createPost);
+router.get("/create",controller.create);
+router.post("/create", 
+upload.single('thumbnail'),
+uploadCloud.upload,
+validate.createPost ,
+controller.createPost);
+
 router.get("/edit/:id",controller.edit);
-
-
 module.exports = router;
